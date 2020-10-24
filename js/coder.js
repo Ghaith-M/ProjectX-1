974ff26a7f78ffaf5010c9726d7bbc6107c42814
Ghaith Madhkour <71493702+Ghaith-M@users.noreply.github.com>
2020-10-24 10:43:18 +0100

    new

--- js/coder.js
+++ js/coder.js
@@ -1,3 +1,6 @@
-$('.datepicker').datepicker({
-    endDate: '-18y'
+$('#btn1').on('click',function(){
+    window.location.href="./index.html"
+  });
+  $('#btn2').on('click',function(){
+    alert("access denied")
   });
